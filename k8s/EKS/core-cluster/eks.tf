7408af5cb9bb1eb89487bf8c984937284e100245
Aditya Shrivastava <aaditech00@gmail.com>
2026-06-30 21:13:37 +0530

    fix: set EKS node group desired/min size to 3 for t3.small capacity
    t3.small has 11-pod limit; need 3 nodes to fit system + monitoring + app pods.

--- k8s/EKS/core-cluster/eks.tf
+++ k8s/EKS/core-cluster/eks.tf
@@ -35,9 +35,9 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
 
-      min_size     = 2
+      min_size     = 3
       max_size     = 5
-      desired_size = 2
+      desired_size = 3
     }
   }
 
